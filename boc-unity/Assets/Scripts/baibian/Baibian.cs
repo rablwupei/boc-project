f0123bb5fc5edffacc508d649855dac1ddb2e350
Wupei <rablwupei@gmail.com>
2018-08-26 15:37:21 +0800

    add dotween

---
+++ boc-unity/Assets/Scripts/baibian/Baibian.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace net.wuyy {
+
+	public enum BaibianType {
+		Tongnian,
+		Haiwai,
+		Chuguo,
+		Chengjia,
+		Zinv,
+		Wannian,
+	}
+
+	public class Baibian : MonoBehaviour {
+
+		public static Baibian instance;
+
+		void Awake() {
+			instance = this;
+		}
+
+		void OnDestroy() {
+			if (instance == this) {
+				instance = null;
+			}
+		}
+
+		public void Change(BaibianType biabian) {
+			
+		}
+
+	}
+
+
+}
---
+++ boc-unity/Assets/Scripts/baibian/UI/LeftPanelClickMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace net.wuyy {
+
+	public class LeftPanelClickMove : MonoBehaviour {
+
+
+
+	}
+
+}
