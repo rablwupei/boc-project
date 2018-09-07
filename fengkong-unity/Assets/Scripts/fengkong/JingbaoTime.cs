5e76ec60c0098be78763bb48084e431a3da8aea5
Wupei <rablwupei@gmail.com>
2018-09-08 00:06:50 +0800

    fix jingbao

---
+++ fengkong-unity/Assets/Scripts/fengkong/JingbaoTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace wuyy.fk {
+
+	public class JingbaoTime : MonoBehaviour {
+
+		public Text text;
+
+		IEnumerator Start() {
+			var time = new WaitForSeconds(1f);
+			while (true) {
+				//9:30:23 AM. SEPT.17TH 2018 A.D.
+				text.text = DateTime.Now.ToString("h:mm:ss tt. MMM.dd\\T\\H yyyy A.D.");
+				yield return time;
+			}
+		}
+
+	}
+
+}
+
--- fengkong-unity/Assets/Scripts/fengkong/UIJingbao.cs
+++ fengkong-unity/Assets/Scripts/fengkong/UIJingbao.cs
@@ -1,10 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace wuyy.fk {
 
 	public class UIJingbao : UIAbstract {
+
+		public CanvasGroup[] hideList;
+
+		public void HideClick() {
+			if (!_isHide) {
+				StartCoroutine(DoHide());
+			}
+		}
+
+		bool _isHide;
+
+		IEnumerator DoHide() {
+			_isHide = true;
+			Tween tween = null;
+			foreach (var item in hideList) {
+				tween = item.DOFade(0f, 0.5f);
+			}
+			if (tween != null) {
+				yield return tween.WaitForCompletion();
+			}
+			Fengkong.instance.Next();
+		}
 		
 	}
 
